e4ab5b4652dce81e3ca2f77d543c714656a4cd8e
leerlandais <leerlandais@gmail.com>
2024-10-22 07:15:59 +0200

    added new, larger json for products

--- public/js/consoleLogger.js
+++ public/js/consoleLogger.js
@@ -7,15 +7,19 @@ if (showTest === undefined || showTest === null) {
     showTest = JSON.parse(localStorage.getItem("TESTS"));
 }
 
+const currentLogSetting = document.getElementById("currentLogSetting");
+currentLogSetting.textContent = showTest;
+
+function toggleLogs(){
+    showTest = !showTest;
+    localStorage.setItem("TESTS", JSON.stringify(showTest));
+    currentLogSetting.textContent = showTest;
+    showTest ? logThis("Logging Activated", true) : null;
+
+}
+
 // Outputs a console.log/console.warn with message and optional expected result
 function logThis(msg, type = false, res = "") {
     type ? console.warn(msg) : console.log(msg);
     res.length > 0 ? console.log("Expected result :", res) : null;
-}
-
-const currentLogSetting = document.getElementById("currentLogSetting");
-function toggleLogs(){
-    showTest = !showTest;
-    currentLogSetting.textContent = showTest;
-
 }
